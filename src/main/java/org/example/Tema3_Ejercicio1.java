0aff8d2ee682d4a4ef9be9eee11c66c768dc3537
Fredy Montaña <fredy8462@hotmail.com>
2022-07-16 06:00:39 -0500

    Se crea la solución al ejercicio 1 del tema 3 - Funciones

---
+++ src/main/java/org/example/Tema3_Ejercicio1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Tema3_Ejercicio1 {
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(suma(7, 8, 9));
+    }
+}
