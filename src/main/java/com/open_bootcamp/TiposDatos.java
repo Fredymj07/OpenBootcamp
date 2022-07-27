8fcf3b8052f9ba92084f89f8b696c48a60c17274
Fredy Montaña <fredy8462@hotmail.com>
2022-07-26 23:14:12 -0500

    Se cargan los archivos correspondientes al ejercicio del tema 1.

---
+++ src/main/java/com/open_bootcamp/TiposDatos.java
@@ -0,0 +1,19 @@
+package com.open_bootcamp;
+
+public class TiposDatos {
+
+    public static void main( String[] args ) {
+
+        String nombre = "Fredy";
+        int edad = 32;
+        long numeroCelular = 311875078;
+        double salario = 4000000;
+        boolean matrimonio = false;
+
+        System.out.println("El tipo de dato STRING tiene el valor -> " + nombre);
+        System.out.println("El tipo de dato INT tiene el valor -> " + edad);
+        System.out.println("El tipo de dato LONG tiene el valor -> " + numeroCelular);
+        System.out.println("El tipo de dato DOUBLE tiene el valor -> " + salario);
+        System.out.println("El tipo de dato BOOLEAN tiene el valor -> " + matrimonio);
+    }
+}
